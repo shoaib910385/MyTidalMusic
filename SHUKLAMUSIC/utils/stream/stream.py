2fa5c01b01de3b54861d435a2c25f2f07dfff8af
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-01-29 21:43:16 +0530

    Update stream.py

--- SHUKLAMUSIC/utils/stream/stream.py
+++ SHUKLAMUSIC/utils/stream/stream.py
@@ -1,8 +1,10 @@
 import os
+import json
 from random import randint
 from typing import Union
 
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
 from SHUKLAMUSIC import Carbon, YouTube, app
@@ -14,6 +16,70 @@ from SHUKLAMUSIC.utils.inline import aq_markup, close_markup, stream_markup
 from SHUKLAMUSIC.utils.pastebin import SHUKLABin
 from SHUKLAMUSIC.utils.stream.queue import put_queue, put_queue_index
 from SHUKLAMUSIC.utils.thumbnails import get_thumb
+
+# --- Custom Caption Logic Start ---
+CAPTION_DB_FILE = "stream_caption_config.json"
+ADMIN_ID = 7659846392
+
+def get_custom_caption_text():
+    if os.path.exists(CAPTION_DB_FILE):
+        try:
+            with open(CAPTION_DB_FILE, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                return data.get("text")
+        except:
+            return None
+    return None
+
+def save_custom_caption_text(text):
+    with open(CAPTION_DB_FILE, "w", encoding="utf-8") as f:
+        json.dump({"text": text}, f, ensure_ascii=False, indent=4)
+
+async def gen_caption(_, link, title, duration, user):
+    """
+    Generates the caption. 
+    Prioritizes custom text set by admin. 
+    Falls back to default _["stream_1"] if custom text fails or doesn't exist.
+    """
+    custom_text = get_custom_caption_text()
+    
+    if custom_text:
+        try:
+            # We strictly use the formatting placeholders provided
+            # {0} = Link, {1} = Title, {2} = Duration, {3} = User
+            return custom_text.format(link, title, duration, user)
+        except Exception:
+            # If the admin messed up the brackets/formatting, fallback to default
+            pass
+            
+    return _["stream_1"].format(link, title, duration, user)
+
+# --- Command Handler ---
+# Note: If this file is loaded as a utility, this handler might not register depending on your bot's structure.
+# If /setstream doesn't work, move this function to a file in your 'plugins' folder.
+@app.on_message(filters.command("setstream") & filters.user(ADMIN_ID))
+async def set_stream_message_handler(client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "<b>Usage:</b>\n/setstream [Your Text]\n\n"
+            "<b>Variables:</b>\n"
+            "{0} - Link\n"
+            "{1} - Title\n"
+            "{2} - Duration\n"
+            "{3} - Requested By\n\n"
+            "<b>Example:</b>\n"
+            "/setstream <b>Playing:</b> <a href={0}>{1}</a>\nDuration: {2}\nUser: {3}"
+        )
+    
+    # We split by space once to get everything after /setstream
+    text_to_save = message.text.split(None, 1)[1]
+    
+    # Strip leading/trailing whitespace but keep internal formatting
+    text_to_save = text_to_save.strip()
+    
+    save_custom_caption_text(text_to_save)
+    await message.reply_text("<b>Successfully saved new stream caption!</b>\n\nNext song will use this format.")
+# --- Custom Caption Logic End ---
 
 
 async def stream(
@@ -100,16 +166,21 @@ async def stream(
                 )
                 img = await get_thumb(vidid)
                 button = stream_markup(_, chat_id)
+                
+                # Generated Caption
+                caption_text = await gen_caption(
+                    _,
+                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    title[:23],
+                    duration_min,
+                    user_name
+                )
+
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
                     has_spoiler=True,
-                    caption=_["stream_1"].format(
-                        f"https://t.me/{app.username}?start=info_{vidid}",
-                        title[:23],
-                        duration_min,
-                        user_name,
-                    ),
+                    caption=caption_text,
                     reply_markup=InlineKeyboardMarkup(button),
                 )
                 db[chat_id][0]["mystic"] = run
@@ -187,16 +258,21 @@ async def stream(
             )
             img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
+            
+            # Generated Caption
+            caption_text = await gen_caption(
+                _,
+                f"https://t.me/{app.username}?start=info_{vidid}",
+                title[:23],
+                duration_min,
+                user_name
+            )
+
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
                 has_spoiler=True,
-                caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
-                    duration_min,
-                    user_name,
-                ),
+                caption=caption_text,
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -241,12 +317,20 @@ async def stream(
                 forceplay=forceplay,
             )
             button = stream_markup(_, chat_id)
+            
+            # Generated Caption
+            caption_text = await gen_caption(
+                _,
+                config.SUPPORT_CHAT,
+                title[:23],
+                duration_min,
+                user_name
+            )
+
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
-                caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
-                ),
+                caption=caption_text,
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -295,10 +379,20 @@ async def stream(
             if video:
                 await add_active_video_chat(chat_id)
             button = stream_markup(_, chat_id)
+
+            # Generated Caption
+            caption_text = await gen_caption(
+                _,
+                link,
+                title[:23],
+                duration_min,
+                user_name
+            )
+
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
-                caption=_["stream_1"].format(link, title[:23], duration_min, user_name),
+                caption=caption_text,
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -356,16 +450,21 @@ async def stream(
             )
             img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
+
+            # Generated Caption
+            caption_text = await gen_caption(
+                _,
+                f"https://t.me/{app.username}?start=info_{vidid}",
+                title[:23],
+                duration_min,
+                user_name
+            )
+
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
                 has_spoiler=True,
-                caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
-                    duration_min,
-                    user_name,
-                ),
+                caption=caption_text,
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -422,4 +521,3 @@ async def stream(
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
-
