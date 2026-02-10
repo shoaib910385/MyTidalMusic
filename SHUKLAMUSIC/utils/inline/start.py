7ddca3507e22430f1fcb68edb57f9e0d35e06626
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-02-11 01:23:23 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -1,48 +1,81 @@
-from pyrogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 import config
-from SHUKLAMUSIC import app
 
-def start_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton("˹ᴘσʟιᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101"),
-            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes"),
-        ],
-    ]
-    return buttons
+def start_panel(_, bot_username: str):
+    kb = InlineKeyboardBuilder()
+
+    kb.row(
+        InlineKeyboardButton(
+            text=_["S_B_1"],
+            url=f"https://t.me/{bot_username}?startgroup=true"
+        ),
+        InlineKeyboardButton(
+            text=_["S_B_2"],
+            url=config.SUPPORT_CHAT
+        ),
+    )
+
+    kb.row(
+        InlineKeyboardButton(
+            text="˹ᴘσʟιᴄʏ˼",
+            url="https://telegra.ph/Privacy-Policy-08-03-101",
+            style="primary"
+        ),
+        InlineKeyboardButton(
+            text="˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪",
+            url="http://t.me/TidalXMusicBot/tidaltunes"
+        ),
+    )
+
+    return kb.as_markup()
 
 
-def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style="secondary"
-            )
-        ],
-        [
-            InlineKeyboardButton("˹ᴘʀιᴠᴧᴄʏ ᴘσʟιᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101"),
-            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes", style="danger"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-        ],
-    ]
-    return buttons
-    
-    
+def private_panel(_, bot_username: str):
+    kb = InlineKeyboardBuilder()
+
+    kb.row(
+        InlineKeyboardButton(
+            text=_["S_B_3"],
+            url=f"https://t.me/{bot_username}?startgroup=true"
+        )
+    )
+
+    kb.row(
+        InlineKeyboardButton(
+            text="˹ᴘʀιᴠᴧᴄʏ ᴘσʟιᴄʏ˼",
+            url="https://telegra.ph/Privacy-Policy-08-03-101"
+        ),
+        InlineKeyboardButton(
+            text="˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪",
+            url="http://t.me/TidalXMusicBot/tidaltunes"
+        ),
+    )
+
+    kb.row(
+        InlineKeyboardButton(
+            text=_["S_B_6"],
+            url=config.SUPPORT_CHANNEL
+        ),
+        InlineKeyboardButton(
+            text=_["S_B_2"],
+            url=config.SUPPORT_CHAT
+        ),
+    )
+
+    kb.row(
+        InlineKeyboardButton(
+            text=_["S_B_4"],
+            callback_data="settings_back_helper"
+        )
+    )
+
+    kb.row(
+        InlineKeyboardButton(
+            text=_["S_B_5"],
+            url=f"https://t.me/{config.OWNER_USERNAME}"
+        )
+    )
+
+    return kb.as_markup()
