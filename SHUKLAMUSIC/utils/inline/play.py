e70d0a729a53f69c3234369fd3a8277320cb0e96
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-02-10 23:39:47 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -4,7 +4,6 @@ from SHUKLAMUSIC import app
 import config
 from SHUKLAMUSIC.utils.formatters import time_to_seconds
 
-
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
         [
@@ -21,6 +20,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style="destructive" # Red color for close
             )
         ],
     ]
@@ -28,61 +28,48 @@ def track_markup(_, videoid, user_id, channel, fplay):
 
 
 def stream_markup_timer(_, chat_id, played, dur):
+    # Progress bar logic remains the same
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "▰▱▱▱▱▱▱▱▱▱"
-    elif 10 < umm < 20:
-        bar = "▰▰▱▱▱▱▱▱▱▱"
-    elif 20 <= umm < 30:
-        bar = "▰▰▰▱▱▱▱▱▱▱"
-    elif 30 <= umm < 40:
-        bar = "▰▰▰▰▱▱▱▱▱▱"
-    elif 40 <= umm < 50:
-        bar = "▰▰▰▰▰▱▱▱▱▱"
-    elif 50 <= umm < 60:
-        bar = "▰▰▰▰▰▰▱▱▱▱"
-    elif 60 <= umm < 70:
-        bar = "▰▰▰▰▰▰▰▱▱▱"
-    elif 70 <= umm < 80:
-        bar = "▰▰▰▰▰▰▰▰▱▱"
-    elif 80 <= umm < 95:
-        bar = "▰▰▰▰▰▰▰▰▰▱" 
-    else:
-        bar = "▰▰▰▰▰▰▰▰▰▰"
-    buttons = [
-                
-        [
-            InlineKeyboardButton(text=" ˹ηєᴛᴡᴏʀᴋ˼ ", url="https://t.me/thedrxnet",),
-            InlineKeyboardButton(text=" ˹ϻʏ ʜᴏϻє˼ ", url="https://t.me/drx_supportchat",),
-        ],
-        
-     [
-            InlineKeyboardButton("˹ᴘʀιᴠᴧᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101"),
-            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes"),
-        ],
+    
+    if 0 < umm <= 10: bar = "▰▱▱▱▱▱▱▱▱▱"
+    elif 10 < umm < 20: bar = "▰▰▱▱▱▱▱▱▱▱"
+    elif 20 <= umm < 30: bar = "▰▰▰▱▱▱▱▱▱▱"
+    elif 30 <= umm < 40: bar = "▰▰▰▰▱▱▱▱▱▱"
+    elif 40 <= umm < 50: bar = "▰▰▰▰▰▱▱▱▱▱"
+    elif 50 <= umm < 60: bar = "▰▰▰▰▰▰▱▱▱▱"
+    elif 60 <= umm < 70: bar = "▰▰▰▰▰▰▰▱▱▱"
+    elif 70 <= umm < 80: bar = "▰▰▰▰▰▰▰▰▱▱"
+    elif 80 <= umm < 95: bar = "▰▰▰▰▰▰▰▰▰▱" 
+    else: bar = "▰▰▰▰▰▰▰▰▰▰"
 
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    buttons = [
+        [
+            InlineKeyboardButton(text=" ˹ηєᴛᴡᴏʀᴋ˼ ", url="https://t.me/thedrxnet", style="success"), # Green
+            InlineKeyboardButton(text=" ˹ϻʏ ʜᴏϻє˼ ", url="https://t.me/drx_supportchat", style="primary"), # Blue
+        ],
+        [
+            InlineKeyboardButton("˹ᴘʀιᴠᴧᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101", style="gray"), # Gray
+            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes", style="warning"), # Orange
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style="destructive")],
     ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
-
-        
         [
-            InlineKeyboardButton(text=" ˹ηєᴛᴡᴏʀᴋ˼ ", url="https://t.me/thedrxnet",),
-            InlineKeyboardButton(text=" ˹ϻʏ ʜᴏϻє˼ ", url="https://t.me/drx_supportchat",),
+            InlineKeyboardButton(text=" ˹ηєᴛᴡᴏʀᴋ˼ ", url="https://t.me/thedrxnet", style="success"), 
+            InlineKeyboardButton(text=" ˹ϻʏ ʜᴏϻє˼ ", url="https://t.me/drx_supportchat", style="primary"),
         ],
         [
-            InlineKeyboardButton("˹ᴘʀιᴠᴧᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101"),
-            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes"),
+            InlineKeyboardButton("˹ᴘʀιᴠᴧᴄʏ˼", url=f"https://telegra.ph/Privacy-Policy-08-03-101", style="gray"),
+            InlineKeyboardButton("˹ᴛιᴅᴧʟ ᴛᴜηєs˼♪", url=f"http://t.me/TidalXMusicBot/tidaltunes", style="warning"),
         ],
-       
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style="destructive")],
     ]
     return buttons
 
@@ -93,16 +80,19 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_1"],
                 callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                style="primary"
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                style="primary"
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style="destructive"
             ),
         ],
     ]
@@ -115,12 +105,14 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_3"],
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+                style="success"
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style="destructive"
             ),
         ],
     ]
@@ -144,14 +136,17 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
             InlineKeyboardButton(
                 text="◁",
                 callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                style="gray"
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {query}|{user_id}",
+                style="destructive"
             ),
             InlineKeyboardButton(
                 text="▷",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                style="gray"
             ),
         ],
     ]
