70ab173a524cb6fdfeeb21658e75ded737cda0bb
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-03-07 16:43:41 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -2,7 +2,6 @@ import math
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from SHUKLAMUSIC import app
 import config
-from pyrogram.enums import ButtonStyle
 from SHUKLAMUSIC.utils.formatters import time_to_seconds
 
 
