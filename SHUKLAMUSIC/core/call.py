373348ed8ca2924eae3d67d0a434fa67c738753b
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-03-07 23:11:16 +0530

    Update call.py

--- SHUKLAMUSIC/core/call.py
+++ SHUKLAMUSIC/core/call.py
@@ -17,7 +17,7 @@ from datetime import datetime, timedelta
 from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
+from pytgcalls import PyTgCalls
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
